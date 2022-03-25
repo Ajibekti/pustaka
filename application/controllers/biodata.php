3cb851b67d1e7c7ce8c064d76f4c0a0f92b516e4
dinddalaras <dinddalarasaty123@gmail.com>
2022-03-25 23:35:14 +0700

    first

--- application/controllers/biodata.php
+++ application/controllers/biodata.php
@@ -4,8 +4,7 @@ class biodata extends CI_Controller
  public function index()
  {
  echo "<h1>Perkenalkan</h1>";
- echo"Saya Siti maymudah tinggal di Cengkareng
- olah raga yang saya sukai adalah
- joging";
+ echo"Saya Adinda Nurul Larasaty tinggal di Kemayoran
+ saya suka BTS";
  }
 }
