9e7808a95b1bf34288a9755abfe7eb5b74da7549
dinddalaras <dinddalarasaty123@gmail.com>
2022-03-25 22:33:40 +0700

    first commit

---
+++ application/controllers/biodata.php
@@ -0,0 +1,12 @@
+<?php
+class biodata extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Aji Subekti
+ Saya tingga di Rusun Cengkareng
+ olah raga yang saya sukai adalah
+ futsal";
+ }
+}
